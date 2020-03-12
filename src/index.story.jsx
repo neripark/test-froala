d2997e73d6b20c16b8922bae525b6155ec3e9dda
neripark <neripark@gmail.com>
2020-03-12 16:04:25 +0900

    first commit

---
+++ src/index.story.jsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+
+
+storiesOf("froala-index", module)
+  .add("default", () => {
+    return <h1>froala page!</h1>
+  });
